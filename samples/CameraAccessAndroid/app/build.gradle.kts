fcc2cdf7f3972eb89f443010a1b36f8381be4397
dennis0405 <dennis0405@snu.ac.kr>
2026-07-06 18:32:04 +0900

    v0.0.1 gradle fix

--- samples/CameraAccessAndroid/app/build.gradle.kts
+++ samples/CameraAccessAndroid/app/build.gradle.kts
@@ -60,12 +60,13 @@ android {
   defaultConfig {
     applicationId = "io.egoflow.app"
     minSdk = 31
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
-    versionName = "0.1.0-alpha.1"
+    versionName = "0.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
+    resourceConfigurations += listOf("en")
   }
 
   signingConfigs {
